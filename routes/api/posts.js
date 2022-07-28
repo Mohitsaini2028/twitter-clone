d7baa0ff9d51ec25b249d9509414a6a2a0ff012a
Mohitsaini2028 <mohitsaini00720@gmail.com>
2022-07-28 23:00:48 +0530

    added like unlike logic

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -46,4 +46,47 @@ router.post("/", async (req, res, next)=>{
     
 })
 
+router.put("/:id/like", async (req, res, next)=>{
+
+    var postId = req.params.id;
+    var userId = req.session.user._id;
+
+    // if post id exist in user likes
+                    // if user likes exist  //then check if user like this post.
+    var isLiked = req.session.user.likes && req.session.user.likes.includes(postId);
+
+    var option = isLiked ? "$pull" : "$addToSet";
+
+    // Insert user like         
+                    // User.findByIdAndUpdate(userId, { [option]: { likes: postId }})     
+    req.session.user = await User.findByIdAndUpdate(userId, { [option]: { likes: postId }}, {new: true})
+    .catch(error => {
+        console.log(error);
+        return res.sendStatus(400);
+    })
+                                                                                            // it will return updated user.
+
+    // User.findByIdAndUpdate(userId, { [option]: { likes: postId }}, function (err, docs) {
+    //     if (err){
+    //         console.log(err)
+    //     }
+    //     else{
+    //         console.log("Updated User : ", docs);
+            
+    //     }
+    // });
+    
+    
+    // Insert post like
+    var post = await Post.findByIdAndUpdate(postId, { [option]: { likes: userId }}, {new: true})
+    .catch(error => {
+        console.log(error);
+        return res.sendStatus(400);
+    })
+    
+    res.status(200).send(post);
+})
+
+
+
 module.exports = router;
--- schemas/PostSchema.js
+++ schemas/PostSchema.js
@@ -6,8 +6,9 @@ const Schema = mongoose.Schema;
 
 const PostSchema = new Schema({
     content: {type: String, trim: true},
-    postedBy: { type: Schema.Types.ObjectId, ref:'User'},
-    pinned: Boolean
+    postedBy: { type: Schema.Types.ObjectId, ref:'User' },
+    pinned: Boolean,
+    likes: [{ type: Schema.Types.ObjectId, ref:'User' }]
 
 },{ timestamps: true });
 
