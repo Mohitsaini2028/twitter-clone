be5f33b40e1396696b7e93e87e329fcc12c100e0
Mohitsaini2028 <mohitsaini00720@gmail.com>
2022-07-28 17:24:40 +0530

    added post icons

--- public/js/common.js
+++ public/js/common.js
@@ -23,6 +23,70 @@ $('#submitPostButton').click((event)=>{
     }
 
     $.post("/api/posts", data, (postData, status, xhr)=>{
-        console.log(postData);
+        
+        var html = createPostHtml(postData);
+        $('.postsContainer').prepend(html);
+        textbox.val("");
+        button.prop("disabled", true);
+        
     })
 });
+
+
+function createPostHtml(postData){
+
+    arr = postData.content.split(" ");
+
+    for (index = 0; index < arr.length; index++) {
+        if(arr[index].startsWith('#')){
+            arr[index]= `<span class='link'>${arr[index]}</span>`;
+        }
+        
+    }
+
+    postData.content = arr.join(' ');
+
+    var postedBy = postData.postedBy;
+    var displayName = postedBy.firstName + " " + postedBy.lastName;
+    var timestamp = postedBy.createdAt;
+
+    return `<div class="post"> 
+
+                <div class="mainContentContainer">
+                    <div class="userImageContainer">
+                        <img src="${postedBy.profilePic}">
+                    </div>
+                    <div class="postContentContainer">
+                        <div class="header">
+                            <a href="/profile/${postedBy.username}" class="displayName">@${displayName}</a>
+                            <span class="username">${postedBy.username}</span>
+                            <span class="date">${timestamp}</span>
+                        </div>
+                        <div class="postBody">
+                        <span>${postData.content}</span>
+                        </div>
+                        <div class="postFooter">
+                            <div class="postButtonContainer">
+                                <button>
+                                    <i class="fa-regular fa-comment"></i>
+                                </button>
+                            </div>
+                            <div class="postButtonContainer">
+                                <button>
+                                <i class="fa-solid fa-retweet"></i>
+                                </button>
+                            </div>
+                            <div class="postButtonContainer">
+                                <button>
+                                    <i class="fa-regular fa-heart"></i>
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+    `;
+    return postData.content;
+
+
+}
